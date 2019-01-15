e8261ddb47e23fea1519737a53640b6d5ce9897a
Ev-1 <33662061+Ev-1@users.noreply.github.com>
2019-01-15 10:32:49 +0100

    Fixed cogs cog

--- cogs/cogs.py
+++ cogs/cogs.py
@@ -10,51 +10,55 @@ class Cogs:
         self.bot = bot
         self.settings = self.bot.settings
 
-    async def __local_check(self, ctx):
-        return await self.bot.is_owner(ctx.author)
-
     @commands.group(name='cogs')
+    @checks.is_owner()
     async def _cogs(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.invoke(self.bot.get_command('help'),
                              ctx.command.qualified_name)
 
     @_cogs.command()
+    @checks.is_owner()
     async def load(self, ctx, *, module):
         """Loads a module."""
         try:
             self.bot.load_extension(f'cogs.{module}')
-            ctx.send(f'{module} loaded')
+            await ctx.send(f'{module} loaded')
         except Exception as e:
             await ctx.send(f'```py\n{traceback.format_exc(e)}\n```')
 
     @_cogs.command()
+    @checks.is_owner()
     async def unload(self, ctx, *, module):
         """Unloads a module."""
         try:
             self.bot.unload_extension(f'cogs.{module}')
-            ctx.send(f'{module} unloaded')
+            await ctx.send(f'{module} unloaded')
         except Exception as e:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
 
     @_cogs.command(name='reload')
+    @checks.is_owner()
     async def _reload(self, ctx, *, module):
         """Reloads a module."""
         try:
             self.bot.unload_extension(f'cogs.{module}')
             self.bot.load_extension(f'cogs.{module}')
-            ctx.send(f'{module} reloaded')
+            await ctx.send(f'{module} reloaded')
         except Exception as e:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
 
     @_cogs.command(name='reloadall')
+    @checks.is_owner()
     async def _relaod_all(self, ctx):
         """Reloads all extensions"""
         try:
             for extension in self.bot.extensions:
-                self.bot.unload_extension(f'cogs.{module}')
-                self.bot.load_extension(f'cogs.{module}')
-            ctx.send('Extensions reloaded')
+                if extension == 'cogs':
+                    continue
+                self.bot.unload_extension(f'cogs.{extension}')
+                self.bot.load_extension(f'cogs.{extension}')
+            await ctx.send('Extensions reloaded')
         except Exception as e:
             await ctx.send(f'```py\n{traceback.format_exc()}\n```')
 
