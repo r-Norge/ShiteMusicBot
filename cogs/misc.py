0fa2314ec268402396104bb730ef7ece6e8ff176
LBlend <3142367-LBlend@users.noreply.gitlab.com>
2019-01-25 03:46:58 +0000

    added more info to the info commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -3,9 +3,11 @@ import os
 import asyncio
 import time
 import random
+import platform
 
 from discord.ext import commands
 from cogs.utils import checks
+from lavalink import __version__ as LavalinkVersion
 
 class Misc:
     def __init__(self, bot):
@@ -38,24 +40,47 @@ class Misc:
         for guild in self.bot.guilds:
             guilds += f"{guild.name}\n"
         await ctx.send(guilds)
-
+    
     @commands.command()
-    async def info(self, ctx, *, channel: str=None):
-        """
-        Info om LovherkBot
-        """
-        avatar = self.bot.user.avatar_url_as(format=None,
-                                             static_format='png',
-                                             size=1024)
-        infotext = f'Shite Music Bot'
-        embed = discord.Embed(color=0xD9C04D)
-        embed.set_author(name=self.bot.user.name, icon_url=avatar)
-        embed.set_thumbnail(url=avatar)
-        embed.add_field(name="Hva?",
-                        value=infotext, inline=False)
-        embed.set_footer(icon_url="https://i.imgur.com/dE6JaeT.gif",
-                         text="Laget av /r/Norge")
-        await ctx.send(embed=embed)
+    async def info(self, ctx):
+    """
+    Info om Shite Music Bot
+    """
+    membercount = []
+    for guild in self.bot.guilds:
+        for member in guild.members:
+            if member.id in membercount:
+                pass
+            else:
+                membercount.append(member.id)
+    guilds = len(self.bot.guilds)
+    members = len(membercount)
+    now = time.time()
+    diff = int(now - self.bot.uptime)
+    days, remainder = divmod(diff, 24 * 60 * 60)
+    hours, remainder = divmod(remainder, 60 * 60)
+    minutes, seconds = divmod(remainder, 60)
+    avatar = self.bot.user.avatar_url_as(format=None,
+                                            static_format='png',
+                                            size=1024)
+    infotext = f'Musikkbot skrevet for bruk på /r/Norge sine Discord Servere. Kildekoden er åpen! Den kan du finne [HER](https://gitlab.com/Ev-1/shite-music-bot)'
+    spectext = f'**Python:** [{platform.python_version()}](https://www.python.org/)\n**Discord.py:** [{discord.__version__}](https://github.com/Rapptz/discord.py/tree/rewrite)\n**Lavalink:** [{LavalinkVersion}](https://github.com/Devoxin/Lavalink.py)'
+    stattext = f'**Guilder:** {guilds}\n**Brukere:** {members}'
+    uptimetext = f'{days}d {hours}t {minutes}m {seconds}s'
+    embed = discord.Embed(color=0xD9C04D)
+    embed.set_author(name=self.bot.user.name, icon_url=avatar)
+    embed.set_thumbnail(url=avatar)
+    embed.add_field(name="Hva?",
+                    value=infotext, inline=False)
+    embed.set_footer(icon_url="https://cdn.discordapp.com/icons/297798952538079233/0198dc7b01ccf288b985b8a394cb1d44.jpg?size=64",
+                        text="Laget av /r/Norge, for /r/Norge")
+    embed.add_field(name="Hvordan?",
+                    value=spectext)
+    embed.add_field(name="Hvor mange?",
+                    value=stattext)
+    embed.add_field(name="Hvor lenge?",
+                    value=uptimetext)
+    await ctx.send(embed=embed)
 
     @commands.command()
     @checks.is_even()
